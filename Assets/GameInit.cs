1176ec59948ea5af7e6693b92f039f432f357210
Alex Schaller <schaller.alexander@protonmail.com>
2018-11-29 15:00:16 +0100

    player spawning implemented according to camera space

--- Assets/GameInit.cs
+++ Assets/GameInit.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class GameInit : MonoBehaviour
 {
     public GameObject Playerblueprint;
+    public int WidthDivisor;
+    public int HeightDivisor;
 
-    private Vector3 spawnpoint0;
     private Vector3 spawnpoint1;
     private Vector3 spawnpoint2;
     private Vector3 spawnpoint3;
+    private Vector3 spawnpoint4;
+
 
 	// Use this for initialization
 	void Start ()
@@ -23,14 +26,43 @@ public class GameInit : MonoBehaviour
             Debug.Log(controller);
 	    }
 
-	    Vector2 cameraMiddle = Camera.main.ScreenToWorldPoint(new Vector2(0.5f, 0.5f));
+	    Camera mainCam = Camera.main;
 
-	    spawnpoint0 = Camera.main.ScreenToWorldPoint(new Vector2(0.1f, 0.9f)); // top right
+	    Vector3 cameraMiddle = mainCam.ScreenToWorldPoint(new Vector2(mainCam.pixelWidth / 2, mainCam.pixelHeight / 2));
+	    cameraMiddle.z = 0.0f;
 
-	    GameObject player = Instantiate(Playerblueprint, spawnpoint0, Quaternion.identity);
-	    player.transform.right = cameraMiddle;
+	    int partWidthIndention = mainCam.pixelWidth / WidthDivisor;
+	    int partHeightIndention = mainCam.pixelHeight / HeightDivisor;
 
-	}
+	    Vector3 bottomLeft = new Vector3(partWidthIndention, partHeightIndention, 0.0f);
+        Vector3 bottomRight = new Vector3(mainCam.pixelWidth - partWidthIndention, partHeightIndention, 0.0f);
+	    Vector3 topLeft = new Vector3(partWidthIndention, mainCam.pixelHeight - partHeightIndention, 0.0f);
+	    Vector3 topRight = new Vector3(mainCam.pixelWidth - partWidthIndention, mainCam.pixelHeight - partHeightIndention, 0.0f);
+
+        spawnpoint1 = mainCam.ScreenToWorldPoint(bottomLeft);
+	    spawnpoint1.z = 0.0f;
+
+	    spawnpoint2 = mainCam.ScreenToWorldPoint(bottomRight);
+	    spawnpoint2.z = 0.0f;
+
+	    spawnpoint3 = mainCam.ScreenToWorldPoint(topLeft);
+	    spawnpoint3.z = 0.0f;
+
+	    spawnpoint4 = mainCam.ScreenToWorldPoint(topRight);
+	    spawnpoint4.z = 0.0f;
+
+	    GameObject player1 = Instantiate(Playerblueprint, spawnpoint1, Quaternion.identity);
+	    player1.transform.up = cameraMiddle - spawnpoint1;
+
+	    GameObject player2 = Instantiate(Playerblueprint, spawnpoint2, Quaternion.identity);
+	    player2.transform.up = cameraMiddle - spawnpoint2;
+
+	    GameObject player3 = Instantiate(Playerblueprint, spawnpoint3, Quaternion.identity);
+	    player3.transform.up = cameraMiddle - spawnpoint3;
+
+	    GameObject player4 = Instantiate(Playerblueprint, spawnpoint4, Quaternion.identity);
+	    player4.transform.up = cameraMiddle - spawnpoint4;
+    }
 	
 	// Update is called once per frame
 	void Update ()
