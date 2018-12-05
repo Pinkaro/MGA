4840bf1263ac055bf0fd831ef3a500ffcdccc9b0
Christoph Cudly <christoph.cudly@gmail.com>
2018-12-05 14:40:30 +0100

    Main Menu

---
+++ Assets/Scripts/Menu/MainMenuController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour {
+
+    public GameObject menu;
+    public GameObject credits;
+
+    public void Start()
+    {
+        credits.SetActive(false);
+        menu.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        Debug.Log("Start");
+    }
+
+    public void ShowCredits()
+    {
+        Debug.Log("Credits");
+        credits.SetActive(true);
+        menu.SetActive(false);
+    }
+
+    public void ShowMenu()
+    {
+        Debug.Log("Menu");
+        menu.SetActive(true);
+    }
+}
