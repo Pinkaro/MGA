020ac20cead91d15d2c87e06e200a40847dc27f7
Christoph Cudly <christoph.cudly@gmail.com>
2018-12-11 20:40:16 +0100

    credits scaler fertig machen

---
+++ Assets/Scripts/Menu/CreditsContentFitter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsContentFitter : MonoBehaviour {
+
+    public void Awake()
+    {
+        Vector2 min = new Vector2();
+        Vector2 max = new Vector2();
+
+        foreach (Transform child in transform)
+        {
+            //Debug.Log(child.GetComponent<RectTransform>().rect.width);
+            //Debug.Log(child.GetComponent<RectTransform>().rect.height);
+            RectTransform rTrans = child.GetComponent<RectTransform>();
+
+            //min
+            if ((child.position.x) < min.x)
+            {
+                min.x = child.position.x;
+            }
+            if ((child.position.y - (rTrans.rect.height/2f)) < min.y)
+            {
+                min.y = child.position.y - (rTrans.rect.height / 2f);
+            }
+
+            //max
+            if (child.position.x < max.x)
+            {
+                max.x = child.position.x;
+            }
+            if ((child.position.y + (rTrans.rect.height / 2f)) > max.y)
+            {
+                max.y = child.position.y + (rTrans.rect.height / 2f);
+            }            
+        }
+
+        Debug.Log("Size X: " + (max.x - min.x));
+        Debug.Log("Size Y: " + (max.y - min.y));
+    }
+
+}
