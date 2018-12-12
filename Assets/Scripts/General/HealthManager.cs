ae412dd0d664126e8c9620bf3516e9162171d456
Zedvia <raffi.papst@gmail.com>
2018-12-12 15:37:06 +0100

    fix healthbar

--- Assets/Scripts/General/HealthManager.cs
+++ Assets/Scripts/General/HealthManager.cs
@@ -11,10 +11,12 @@ public class HealthManager : MonoBehaviour
 
     private SpriteRenderer spriteRenderer;
     public GameObject Healthbar;
+    private SpriteRenderer healthbarSpriteRenderer;
 
     void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        healthbarSpriteRenderer = Healthbar.GetComponent<SpriteRenderer>();
     }
 
 	// Update is called once per frame
@@ -30,22 +32,23 @@ public class HealthManager : MonoBehaviour
     {
         Health += effect;
 
+        if (Healthbar != null)
+        {
+            if (Health > maxHealth)
+            {
+                Healthbar.transform.localScale = new Vector3(1, 1, 1);
+            }
+            else
+            {
+                var scale = Healthbar.transform.localScale;
+                scale.x = Health / maxHealth;
+                Healthbar.transform.localScale = scale;
+                //healthbarSpriteRenderer.doSmth();
+            }
+        }
+
         if (Health <= 0.0f)
         {
-            if (Healthbar != null)
-            {
-                if (Health > maxHealth)
-                {
-                    Healthbar.transform.localScale = new Vector3(1, 1, 1);
-                }
-                else
-                {
-                    var scale = Healthbar.transform.localScale;
-                    scale.x = Health / maxHealth;
-                    Healthbar.transform.localScale = scale;
-                }
-            }
-
             var killables = transform.GetComponents<IKillable>().ToList();
 
             foreach (var killable in killables)
