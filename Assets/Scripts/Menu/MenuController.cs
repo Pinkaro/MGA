abb5a98ee34aa0941a83437e7172f8208f761526
Christoph Cudly <christoph.cudly@gmail.com>
2018-12-12 13:52:53 +0100

    MenuController

---
+++ Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+
+    static string player1;
+
+    public static void SetPlayerID(string id)
+    {
+        player1 = id;
+    }
+}
